dcd1ee6e0ea764265bfe8f80afaf1e46b86908c0
Ankush <ankushpanchal144@gmail.com>
2021-08-07 23:07:13 +0530

    made website responsive

--- js/app.js
+++ js/app.js
@@ -5,5 +5,18 @@ $(document).ready(function(){
         slidesToScroll:1,
         prevArrow:".prev-btn",
         nextArrow:".next-btn",
+        responsive :[
+            {
+                breakpoint:992,
+                settings:{
+                    slidesToShow:1,
+                }
+            }
+        ]
     });
+    $('.nav-trigger').click(function(){
+        $('.site-content-wrapper').toggleClass('scaled');
+
+    })
 });
+
